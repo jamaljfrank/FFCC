71b885f7c7098f926d12f28a28a744f2cf108f0b
jamaljfrank <jamaljfrank@gmail.com>
2020-05-04 14:50:51 -0700

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+
     has_many :characters
     has_many :adventures
     has_many :played_adventures, through: :characters,
         source: :adventure
     has_secure_password
+    
+    validates :email, presence: true
+    validates :email, uniqueness: true
 end
