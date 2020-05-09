d3d12a71125b66da509f69222c6327e0d1b087dc
jamaljfrank <jamaljfrank@gmail.com>
2020-05-09 14:24:54 -0700

    added validations to character

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,9 @@
 class Character < ApplicationRecord
     belongs_to :user
     belongs_to :adventure
+
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    validates :tribe, presence: true
+    validates :tribe, uniqueness: true
 end
