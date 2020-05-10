cf04aefdd965044be557ec04575c9365daf8b9fa
jamaljfrank <jamaljfrank@gmail.com>
2020-05-09 18:37:33 -0700

    Added new view to character controller

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,6 +1,30 @@
 class CharactersController < ApplicationController
 
+    TRIBES = ["Clavat", "Lilty", "Selkie", "Yuke"]
+
     def index
         @characters = Character.led_by(current_user.id)
     end
+
+    def new 
+        @character = Character.new
+    end
+
+    def create 
+        @character = current_user.characters.build(character_params)
+        if @character.save 
+            redirect_to characters_path(current_user)
+        else 
+            # Error message
+            render 'new'
+        end 
+    end
+
+    private
+
+    def character_params 
+        params.require(:character).permit(:name, :tribe, :avatar)
+    end
+
+    
 end
