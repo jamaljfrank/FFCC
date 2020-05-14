828b6bff744902cdb7fbcfe2d64eea8caccf0f21
jamaljfrank <jamaljfrank@gmail.com>
2020-05-14 03:26:23 -0700

    just binding around.

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -2,8 +2,8 @@ class BattlesController < ApplicationController
 
   def create
     @battle = current_adventure.battles.build(battle_params)
-    
     @battle.character_id = current_user.characters.first.id
+    
     redirect_to root_path, :notice => @battle.results
   end
 
